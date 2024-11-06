120e82cbd9ed473a375cb0cbd3eee62f13d051b9
Matthias Heitmann <matthias.heitmann@dreckscoding.de>
2024-11-06 07:28:07 +0100

    data: updated inkdecks data

--- results/inkdecks/extractor.js
+++ results/inkdecks/extractor.js
@@ -3,7 +3,7 @@ import IndecksExtractor from "./IndecksExtractor.js";
 // start extractor with default options
 const options = {
   fetchTournaments: true,
-  numberOfPages: 16,
+  numberOfPages: 20,
   fetchDecklists: true,
   fetchCards: true,
   persistOutput: true,
