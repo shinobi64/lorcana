0baf7d7fc08edfc302e9126316c2f6df1f61848b
Matthias Heitmann <shinobi64@users.noreply.github.com>
2025-02-03 12:39:47 +0100

    data: inkdecks 2025-02-02

--- results/inkdecks/extractor.js
+++ results/inkdecks/extractor.js
@@ -3,7 +3,7 @@ import IndecksExtractor from "./IndecksExtractor.js";
 // start extractor with default options
 const options = {
   fetchTournaments: true,
-  numberOfPages: 80,
+  numberOfPages: 82,
   fetchDecklists: true,
   fetchCards: true,
   persistOutput: true,
