c040028f4928ba55f4d073cbc380e71287a3ac49
Matthias Heitmann <matthias.heitmann@dreckscoding.de>
2026-08-18 09:15:31 +0200

    fix: cleanup old code

--- rph/src/main.ts
+++ rph/src/main.ts
@@ -13,40 +13,10 @@ async function run() {
     tournamentConfig.eventName,
   );
   await eventInstance.fetchInformation();
-  // eventInstance.showInformation();
   eventInstance.showEventRecord();
   eventInstance.showEventRankings();
   eventInstance.showEventMatches();
   eventInstance.showEventReport();
-
-  /**
-  logger.logInfo("======= TOURNAMENT MAIN DATA =======");
-  const tournamentData = await fetchData(`events/${tournamentConfig.eventid}/`);
-  logger.logInfo(JSON.stringify(tournamentData, null, 2));
-  logger.logInfo("======= REGISTRATIONS =======");
-  const tournamentRegistrations = await fetchData(
-    `events/${tournamentConfig.eventid}/registrations`,
-  );
-  logger.logInfo(JSON.stringify(tournamentRegistrations, null, 2));
-  tournamentData.tournament_phases.forEach(async (phase) => {
-    phase.rounds.forEach(async (round) => {
-      logger.logInfo(
-        `======= PHASE ${phase.phase_name} - ROUND ${round.round_number} PAIRINGS =======`,
-      );
-      const roundPairings = await fetchData(
-        `tournament-rounds/${round.id}/matches/paginated/?page=1&page_size=20`,
-      );
-      logger.logInfo(JSON.stringify(roundPairings, null, 2));
-      logger.logInfo(
-        `======= PHASE ${phase.phase_name} - ROUND ${round.round_number} STANDINGS =======`,
-      );
-      const roundStandings = await fetchData(
-        `tournament-rounds/${round.id}/standings/paginated/?page=1&page_size=20`,
-      );
-      logger.logInfo(JSON.stringify(roundStandings, null, 2));
-    });
-  });
-   */
 }
 
 run();
